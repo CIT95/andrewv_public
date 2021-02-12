0f2c85e7295bc47ae25759be7c6be7a9f4113779
andrew valenzuela <andrew.f.valenzuela@gmail.com>
2021-02-12 08:13:04 -0800

    learntogether week 5

---
+++ w5/list_exercises.py
@@ -0,0 +1,97 @@
+# got this one
+lst=[11, 100, 99, 1000, 999]
+answer_1=lst[0]
+
+print(answer_1)
+# got this one
+lst=[11, 100, 101, 999, 1001]
+
+print(lst[1])
+# got this one
+lst=[11, 100, 101, 999, 1001]
+#Type your answer here.
+
+answer_1=lst[-1]
+
+print(answer_1)
+
+# got this one
+gift_list=['socks', '4K drone', 'wine', 'jam']
+# Type your code here.
+gift_list.append('pajamas')
+
+
+print(gift_list)
+# got this one
+gift_list=['socks', '4K drone', 'wine', 'jam']
+# Type your code here.
+gift_list.append(["socks", "tshirt", "pajamas"] )
+
+
+print(gift_list)
+# got this one wrong the first time because i used 3 instead of 2
+gift_list=['socks', '4K drone', 'wine', 'jam']
+# Type your code here.
+
+gift_list.insert(2,'slippers')
+
+print(gift_list)
+# got this one
+
+lst=[55, 777, 54, 6, 76, 101, 1, 2, 8679, 123, 99]
+#  Type your code here.
+
+answer_1= lst.index(8679)
+
+print(answer_1)
+# got this one
+lst=["CRV", "Outback", "XC90", "GL", "Cherokee", "Escalade"]
+#  Type your code here.
+lst.append(['Navigator', 'Suburban'])
+
+
+print(lst)
+# got this one
+# i check the solution after and they put in 99 but i use a negative index to get the last element
+lst=[55, 777, 54, 6, 76, 101, 1, 2, 8679, 123, 99]
+#  Type your code here.
+
+lst.remove(lst[-1])
+
+print(lst)
+# got this one
+lst=[55, 777, 54, 6, 76, 101, 1, 2, 8679, 123, 99]
+#  Type your code here.
+
+lst.reverse()
+
+print(lst)
+
+# got this one
+lst=[55, 6, 777, 54, 6, 76, 101, 1, 6, 2, 6]
+#  Type your code inside print() function.
+
+answer_1= lst.count(6)
+
+print(answer_1)
+# got this one
+lst=[55, 6, 777, 54, 6, 76, 101, 1, 6, 2, 6]
+
+#  Type your code on line 4:
+answer_1= sum(lst)
+
+print(answer_1)
+# this one i had to check the solution because the instructions say to use Max, and i thought there could be a second arg i didnt know about that would give the min
+lst=[55, 6, 777, 54, 6, 76, 101, 1, 6, 2, 6]
+
+#  Type your code on line 4:
+answer_1= min(lst)
+
+print(answer_1)
+# got this one
+lst=[55, 6, 777, 54, 6, 76, 101, 1, 6, 2, 6]
+
+#  Type your code on line 4:
+answer_1= max(lst)
+
+print(answer_1)
